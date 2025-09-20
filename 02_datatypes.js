641dcbb3c073a70ce027f41a427c1335666bc894
A-Dube <dube.aakarsan@gmail.com>
2025-09-21 01:46:08 +0530

    Basic

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  //Treats all JS Code as newer version
+
+//alert (3+3).....we are using node.js not browser
+
+console.log(3+3)
+console.log("Dube")
+
+
+let name="Ansh"
+let age=20
+let isLoggedIn=false
+let state=null
+
+//number=> 2^53
+//bigint
+//string=""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol
+
+//object
+
+console.log(typeof state)
+console.log(typeof undefined)
